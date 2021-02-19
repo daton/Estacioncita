8610357b3482b6862393ccaf078ad38312812a9c
daton <rapidclimate@gmail.com>
2021-02-19 14:54:47 -0600

    Initial commit

---
+++ app/src/main/java/com/unitec/estacioncita/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.unitec.estacioncita
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Log.i
+import android.util.LruCache
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var url="https://api.weatherlink.com/v1/NoaaExt.json?user=001D0A0040AE&pass=Campitos1973&apiToken=8DA86B5995E94BA3BD3EBD811086F906"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+
+   //La temperatura
+           texto.text=     response.get("temp_c").toString()
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+                texto.text = "Hubo un error, ${error}"
+            }
+        )
+
+        // Acceso al request por medio de una clase Singleton
+        MiSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
+
+    }
+
+
+
+}
+
+
+class MiSingleton constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: MiSingleton? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: MiSingleton(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+
+    //Para el caso d cargar un objeto como una imagen.
+    val imageLoader: ImageLoader by lazy {
+        ImageLoader(requestQueue,
+            object : ImageLoader.ImageCache {
+                private val cache = LruCache<String, Bitmap>(20)
+                override fun getBitmap(url: String): Bitmap {
+                    return cache.get(url)
+                }
+                override fun putBitmap(url: String, bitmap: Bitmap) {
+                    cache.put(url, bitmap)
+                }
+            })
+    }
+    val requestQueue: RequestQueue by lazy {
+        // applicationContext es para evitar fuga de mmoria
+
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
---
+++ app/src/main/java/com/unitec/estacioncita/Usuario.kt
@@ -0,0 +1,9 @@
+package com.unitec.estacioncita
+
+
+
+
+class Usuario {
+   var email:String?=null
+  var nombre:String?=null
+}
